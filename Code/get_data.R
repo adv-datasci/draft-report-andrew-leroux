3e8599053fca573a1049b354cfb4461c2468961c
Andrew Leroux <aleroux2@jhu.edu>
2017-09-19 18:28:53 -0400

    fixed code to look across multiple .R files for run_analysis.R

--- Code/get_data.R
+++ Code/get_data.R
@@ -9,9 +9,7 @@ token <- readLines("../AdvDataScience_Project1/github_token.txt")[1]
 
 ### Only 100 results per page (the max). 
 ### Search by range of dates created -- 1 week periods. Should be able to grab them all
-
-
-date_start <- ymd("2015-01-01")        ## start date
+date_start <- ymd("2016-01-01")        ## start date
 day_inc    <- 14                       ## increment days by 14 at a time
 dates <- c(); i <- 1
 while(date_start < Sys.Date() - (day_inc+1)) {
@@ -22,13 +20,11 @@ while(date_start < Sys.Date() - (day_inc+1)) {
 rm(list=c("date_start","i","day_inc"))
 
 
-print(length(dates))
-
-repos <- c()
+repos <- date_repo <- c()
 for(i in 1:length(dates)){
-        page <- 1
-        x <- c()
-        gh_date <- paste("created:", paste(dates[[i]], collapse=".."), sep="")
+        gh_date        <- paste("created:", paste(dates[[i]], collapse=".."), sep="")
+        repo_len_start <- length(repos)
+        
         ## get all repo names
         for(k in 1:10){
                 gh_get <- paste("GET /search/repositories?q=getting+and+cleaning+data+",gh_date, "&per_page=100", sep="")
@@ -39,27 +35,55 @@ for(i in 1:length(dates)){
                 repos <- c(repos, vapply(x[[3]], "[[", character(1), "full_name"))
                 
         }
-        Sys.sleep(60)
+        delta_repos <- length(repos) - repo_len_start
+        date_repo   <- c(date_repo, rep(dates[[i]][1], delta_repos))
+        
+        Sys.sleep(120)
         print(i)
 }
+rm(list=c("delta_repos","gh_get","x","repo_len_start","gh_date","i"))
+
+
+## create empty list to hold data
+code <- list()
+for(i in 1:length(repos)){
+        code[[i]] <- NA
+}
+
 
 ## loop over recovered repos to get run_analysis.R
-for(i in repos){
+for(i in length(repos)){
         repo <- repos[i]
         string <- paste0("GET /search/code?q=repo:", repo,"+extension:r")
         res <- gh::gh(string, .token=token)
         
+        ## loop over this -- look for some variant of run_analysis.R
         path <- try(res[[3]][[1]]$path)
         
         if("try-error" %in% class(path)) next
-        code.url <- file.path("https://raw.githubusercontent.com",repo, "master", path)
         
-        code <- code.url %>% readLines()
-        head(code)
+        for(k in 1:paths_len){
+                path_cur  <- res[[3]][[k]]$path
+                file_name_inx <- c(gregexpr("/[aA-zZ]+?.[rR]",path_cur)[[1]][1], nchar(path_cur))
+                file_name <- substr(path_cur, file_name_inx[1]+1, file_name_inx[2])
+                
+                if(tolower(file_name) != "run_analysis") next
+                
+                has_code <- TRUE
+                
+        }
+        
+        if(!has_code) next
+        
+        code.url <- file.path("https://raw.githubusercontent.com",repo, "master", path_cur)
+        code[[i]] <- readLines(code.url)
+        
         
 }
 
 
+
+        
 
 
 
